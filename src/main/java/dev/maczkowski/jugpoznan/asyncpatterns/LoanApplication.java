1a52c73ddbb9de09798bce4f265bf7aae82a23e8
Mariusz Mączkowski <mari6274@wp.pl>
2025-01-17 22:18:38 +0100

    example mistake

--- src/main/java/dev/maczkowski/jugpoznan/asyncpatterns/LoanApplication.java
+++ src/main/java/dev/maczkowski/jugpoznan/asyncpatterns/LoanApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+SpringBootApplication
 public class LoanApplication {
     public static void main(String[] args) {
         SpringApplication.run(LoanApplication.class, args);
